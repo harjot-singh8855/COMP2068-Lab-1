713328b86f60ef641a3b8fabd19783b4820af744
harjot-singh8855 <64723180+harjot-singh8855@users.noreply.github.com>
2020-05-28 22:55:12 -0400

    hii first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+const http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(300, {
+        "Content-Type": "text/plain"
+    });
+    res.end('Hello World!');
+}).listen(3000);
+console.log('Hello world');
